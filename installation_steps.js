a718fd44d6739b23b16ef33fc70e6c0e7a3a1618
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-01-22 18:42:14 +0200

    installation steps

---
+++ installation_steps.js
@@ -0,0 +1,63 @@
+Community , Entrprise , Atlas
+
+
+1- DB server installation
+	server
+	 https://www.mongodb.com/try/download/community
+	
+	unckeck   service , compass
+	
+2- DB tools installation
+	tools
+	https://www.mongodb.com/try/download/database-tools
+	
+	install MongoSH clinet zip for cersion 6
+	https://www.mongodb.com/try/download/shell
+
+
+documentation
+https://docs.mongodb.com/
+	
+3- add bin path for (tools and server nad mongosh)  
+to the environment varaibles of OS
+
+
+4- open mongoDB server --> cmd --> mongod   --> localhost:27017
+
+5-  create default folder c:\data\db
+
+javascript shell--> JS Engine  spiderMonky
+6- client   --> 
+types :   1- cmd --> mongo (5 or 4)  || mongosh  (6) 		
+		  2- GUI tool    Robo3T (noSqlBooster)
+		  3- Driver (library)
+
+
+on server --> wiredTiger Engine --> save data as BSON (Binary JSON)
+fast reading
+additinal JS dataTypes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
